bf57283f23460ed10e22114fd432b18a8c38557a
joseph <joseph.hangarter@yahoo.com>
2019-03-21 15:19:49 -0700

    got some functionality to work

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -8,15 +8,20 @@ var Cart = function(items) {
 
 Cart.prototype.addItem = function(product, quantity) {
   // TODO: Fill in this instance method to create a new CartItem and add it to this.items
+  var new_item = new CartItem(product, quantity);
+  this.items.push(new_item);
 };
 
 Cart.prototype.saveToLocalStorage = function() {
   // TODO: Fill in this instance method to save the contents of the cart to localStorage
+  var stringy_items = JSON.stringify(items);
+  localStorage.setItem('items', stringy_items);
 };
 
 Cart.prototype.removeItem = function(item) {
   // TODO: Fill in this instance method to remove one item from the cart.
   // Note: You will have to decide what kind of parameter to pass in here!
+  this.items.splice(item, 1);
 };
 
 var CartItem = function(product, quantity) {
